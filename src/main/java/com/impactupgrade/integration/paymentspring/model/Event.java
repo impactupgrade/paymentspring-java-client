c61c921da831e8e118596303267b1a1c8155adf1
Brett Meyer <brett@3riverdev.com>
2020-04-20 10:45:36 -0400

    additional, bare objects

---
+++ src/main/java/com/impactupgrade/integration/paymentspring/model/Event.java
@@ -0,0 +1,37 @@
+package com.impactupgrade.integration.paymentspring.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Event implements AbstractModel {
+
+  @JsonProperty("event_resource")
+  private String eventResource;
+  @JsonProperty("event_type")
+  private String eventType;
+  @JsonProperty("payload")
+  private String payloadJson;
+
+  public String getEventResource() {
+    return eventResource;
+  }
+
+  public void setEventResource(String eventResource) {
+    this.eventResource = eventResource;
+  }
+
+  public String getEventType() {
+    return eventType;
+  }
+
+  public void setEventType(String eventType) {
+    this.eventType = eventType;
+  }
+
+  public String getPayloadJson() {
+    return payloadJson;
+  }
+
+  public void setPayloadJson(String payloadJson) {
+    this.payloadJson = payloadJson;
+  }
+}
---
+++ src/main/java/com/impactupgrade/integration/paymentspring/model/Subscription.java
@@ -0,0 +1,25 @@
+package com.impactupgrade.integration.paymentspring.model;
+
+import java.util.Map;
+
+public class Subscription implements AbstractModel {
+
+  private String id;
+  private Map<String, String> metadata;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public Map<String, String> getMetadata() {
+    return metadata;
+  }
+
+  public void setMetadata(Map<String, String> metadata) {
+    this.metadata = metadata;
+  }
+}
