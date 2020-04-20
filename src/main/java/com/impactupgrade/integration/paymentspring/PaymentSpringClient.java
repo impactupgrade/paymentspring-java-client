ec066fb31722733d732bb2e634c2179d97f25a26
Brett Meyer <brett@3riverdev.com>
2020-04-20 10:55:50 -0400

    wrapper client

---
+++ src/main/java/com/impactupgrade/integration/paymentspring/PaymentSpringClient.java
@@ -0,0 +1,14 @@
+package com.impactupgrade.integration.paymentspring;
+
+public class PaymentSpringClient {
+
+  private final String apiKey;
+
+  public PaymentSpringClient(String apiKey) {
+    this.apiKey = apiKey;
+  }
+
+  public CustomersClient customers() {
+    return new CustomersClient(apiKey);
+  }
+}
