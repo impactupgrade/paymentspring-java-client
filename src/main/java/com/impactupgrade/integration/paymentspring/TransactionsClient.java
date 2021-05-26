d9109d2d25659608c57be8a3b3a388a37e6fdc76
Brett Meyer <brett@3riverdev.com>
2021-05-26 16:57:17 -0400

    reworking getTransactionsBetweenDates

--- src/main/java/com/impactupgrade/integration/paymentspring/TransactionsClient.java
+++ src/main/java/com/impactupgrade/integration/paymentspring/TransactionsClient.java
@@ -3,14 +3,13 @@ package com.impactupgrade.integration.paymentspring;
 import com.impactupgrade.integration.paymentspring.model.Transaction;
 import com.impactupgrade.integration.paymentspring.model.TransactionList;
 
-import java.util.List;
-
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Form;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 public class TransactionsClient extends AbstractClient {
 
@@ -29,7 +28,11 @@ public class TransactionsClient extends AbstractClient {
         .get(Transaction.class);
   }
 
-  public TransactionList getTransactionsBetweenDates(String startDate, String endDate, int limit, int page) {
+  public List<Transaction> getTransactionsBetweenDates(String startDate, String endDate) {
+    return getTransactionsBetweenDates(startDate, endDate, 100, 1);
+  }
+
+  private List<Transaction> getTransactionsBetweenDates(String startDate, String endDate, int limit, int page) {
     Form form = new Form();
     form.param("start_date", startDate).param("end_date", endDate);
     
@@ -41,8 +44,18 @@ public class TransactionsClient extends AbstractClient {
         .header(HttpHeaders.AUTHORIZATION, bearerToken)
         .post(Entity.form(form));
 
-    return response.readEntity(TransactionList.class);
+    TransactionList transactionList = response.readEntity(TransactionList.class);
+    List<Transaction> transactions = transactionList.getList();
 
+    // Number of pages needed
+    int totalResults = transactionList.getMeta().getTotalResults();
+    int totalPages = (totalResults / limit) + 1;
 
+    // If more pages need to get all possible results, recursive call
+    if (page <= totalPages) {
+      transactions.addAll(getTransactionsBetweenDates(startDate, endDate, limit, page + 1));
+    }
+
+    return transactions;
   }
 }
